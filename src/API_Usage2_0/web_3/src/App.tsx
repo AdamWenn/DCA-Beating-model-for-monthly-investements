e5a3ff44062cd5d0a6607b89aeab296b22520aa2
adawe346 <adawe346@student.liu.se>
2025-10-01 00:45:31 +0200

    Great looking website

---
+++ src/API_Usage2_0/web_3/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// Import the component from web_3/components
+import ImmersiveAuroraEvidence from '../components/ImmersiveAurora'
+
+export default function App(){
+  return (
+    <div style={{ minHeight:'100vh', color:'#e5e7eb' }}>
+      <ImmersiveAuroraEvidence />
+    </div>
+  )
+}
+
